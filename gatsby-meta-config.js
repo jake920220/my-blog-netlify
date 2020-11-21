ecc6ce0fc26ab6ee8dfaa18b857ac1e2601d231a
jake920220 <jake220@naver.com>
2020-11-22 01:05:23 +0900

    ADD: GA 측정 코드 추가
    기존에는 추적코드인데 ga4에서 측정코드로 바뀌었는데 연동이 잘 될지 모름.

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -26,5 +26,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'G-YDX7JJD5M5', // Add your google analytics tranking ID
 }
