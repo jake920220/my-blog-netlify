6101d33efce8e9c816da59335915865584af85b1
jake920220 <linokim220@gmail.com>
2023-04-26 17:08:37 +0900

    [fix] 포스트 날짜 추가

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -9,6 +9,7 @@ export const ThumbnailItem = ({ node }) => (
     <div key={node.fields.slug}>
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      <span>{node.frontmatter.date}</span>
     </div>
   </Link>
 )
